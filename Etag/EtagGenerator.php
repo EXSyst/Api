378e87ef2eaec878b142a14eaf49ab85ea92672d
Ener-Getick <energetick.guigui@gmail.com>
2015-10-01 21:32:46 +0200

    Simplify the EtagGenerator

--- Etag/EtagGenerator.php
+++ Etag/EtagGenerator.php
@@ -26,72 +26,54 @@ class EtagGenerator
      */
     public function generate($value, $strategy = EtagStrategy::PREFER_STRONG)
     {
-        list($etag, $strategy) = $this->processField($value, $strategy);
-        if ($strategy === EtagStrategy::PREFER_WEAK) {
-            $etag->setWeak(true);
+        if ($value instanceof Etag) {
+            return $value;
+        } elseif (is_scalar($value)) {
+            return new Etag($value);
+        } elseif (is_array($value)) {
+            return $this->processArray($value, $strategy);
+        } elseif ($value instanceof EtaggableInterface) {
+            return $this->processEtaggableObject($value, $strategy);
         }
 
         return $etag;
     }
 
     /**
-     * @param mixed $value
+     * @param array $value
      * @param int   $strategy
      *
-     * @return array|null
+     * @return Etag
      */
-    private function processField($value, $strategy)
+    private function processArray(array $value, $strategy)
     {
-        if ($value instanceof Etag) {
-            return [$value, $strategy];
-        } elseif (is_scalar($value)) {
-            return [new Etag($this->hash($value)), $strategy];
-        } elseif (is_array($value)) {
-            $newValue = '';
-            $newStrategy = $strategy;
-            $weak = false;
-            foreach ($value as $k => $v) {
-                list($etag, $tempStrategy) = $this->processField($v, $strategy);
-                $newStrategy = $this->compareStrategies($tempStrategy, $newStrategy);
-
-                $weak = $weak || $etag->isWeak();
-                $newValue .= $this->hash(
-                    $this->hash($k).self::VALUE_SEPARATOR.$this->hash($etag->getValue())
-                );
-            }
-        } elseif ($value instanceof EtaggableInterface) {
-            if ($value instanceof ExtendedEtaggableInterface) {
-                if ($strategy === EtagStrategy::DONT_CARE &&
-                    (!$value->supportsStrongEtag() || $value->prefersWeakEtag())) {
-                    $strategy = EtagStrategy::PREFER_WEAK;
-                } elseif ($strategy === EtagStrategy::PREFER_STRONG && !$value->supportsStrongEtag()) {
-                    $strategy = EtagStrategy::PREFER_WEAK;
-                }
-            }
-            $value = $value->getEtag($strategy);
-            if (!($value instanceof Etag)) {
-                $value = new Etag($value);
-            }
-
-            return [$value, $strategy];
+        $newValue = '';
+        $weak = false;
+        foreach ($value as $k => $v) {
+            $etag = $this->processField($v, $strategy);
+            $weak = $weak || $etag->isWeak();
+            $newValue .= $this->hash(
+                $this->hash($k).self::VALUE_SEPARATOR.$this->hash($etag->getValue())
+            );
         }
+
+        return new Etag($newValue, $weak);
     }
 
     /**
-     * @param int $a
-     * @param int $b
+     * @param EtaggableInterface $value
+     * @param int                $strategy
      *
-     * @return int
+     * @return Etag
      */
-    private function compareStrategies($a, $b)
+    private function processEtaggableObject(EtaggableInterface $value, $strategy)
     {
-        if ($b === EtagStrategy::DONT_CARE) {
-            return $a;
-        } elseif ($a === EtagStrategy::DONT_CARE || $a === EtagStrategy::PREFER_STRONG) {
-            return $b;
-        } else {
-            return $a;
+        $value = $value->getEtag($strategy);
+        if (!($value instanceof Etag)) {
+            $value = new Etag($value, true);
         }
+
+        return $value;
     }
 
     /**
