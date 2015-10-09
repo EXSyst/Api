9adfe7d1086620103f1bb473e3b8c415073c8dca
Ener-Getick <energetick.guigui@gmail.com>
2015-10-09 20:35:46 +0200

    Add a copyright header

--- Tests/Parameter/ParameterReaderTest.php
+++ Tests/Parameter/ParameterReaderTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Rest package.
+ *
+ * (c) EXSyst
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace EXSyst\Component\Rest\Tests\Parameter;
 
 use Doctrine\Common\Annotations\AnnotationReader;
